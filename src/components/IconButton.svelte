bb861025e8cda30460792ad6899588532ab16cb7
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-08-22 15:32:22 -0700

    :construction: first button iteration

---
+++ src/components/IconButton.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let innerText = "";
+	export let svg = "";
+</script>
+
+<button>
+	<img src={svg} alt="button icon" />
+	{innerText}
+</button>
+
+<style>
+	button {
+		display: flex;
+		gap: 0.5rem;
+		justify-content: center;
+		align-items: center;
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+	}
+
+	button:hover {
+		cursor: pointer;
+	}
+
+	button img {
+		width: 2rem;
+	}
+</style>
