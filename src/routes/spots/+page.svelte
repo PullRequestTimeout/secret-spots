e41c919b6a7431f231c9c78686697627e1ee03fb
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-08-30 21:03:37 -0700

    :fire: Removed logout button after menu functional

--- src/routes/spots/+page.svelte
+++ src/routes/spots/+page.svelte
@@ -2,12 +2,3 @@
 	import { authHandlers } from "$lib/stores/store";
 	import IconButton from "$lib/components/IconButton.svelte";
 </script>
-
-<!-- <button class="btn btn-red" on:click={authHandlers.logout}>Logout</button> -->
-
-<IconButton
-	className={"btn-red"}
-	callback={authHandlers.logout}
-	svg={"/icons/logout_icon.svg"}
-	innerText={"Logout"}
-/>
