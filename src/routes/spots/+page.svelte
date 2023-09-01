91a80c4ffdc089a00e00bc8cc36fadc3bfa0fdad
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-08-31 21:18:58 -0700

    :construction: bare beginnings of the spots page

--- src/routes/spots/+page.svelte
+++ src/routes/spots/+page.svelte
@@ -1,6 +1,60 @@
 <script>
-	import { getAuth } from "firebase/auth";
+	import { auth } from "$lib/firebase/firebase";
+	import { onMount } from "svelte";
+	import Loading from "$lib/components/Loading.svelte";
+	import IconButton from "$lib/components/IconButton.svelte";
+	import { fade } from "svelte/transition";
 
-	const auth = getAuth();
-	console.log(auth.currentUser);
+	let loading = true;
+	let displayName;
+
+	onMount(() => {
+		auth.onAuthStateChanged(() => {
+			console.log(auth.currentUser);
+			displayName = auth.currentUser.displayName;
+			loading = false;
+		});
+	});
 </script>
+
+<main>
+	{#if loading}
+		<!-- Loading animation -->
+		<Loading />
+	{:else if !loading}
+		<div transition:fade={{ duration: 200 }}>
+			<h2>Welcome, {displayName}!</h2>
+			<p>Add a spot to get started</p>
+			<IconButton svg={"/icons/add_icon.svg"} innerText={"Add Spot"} className={"btn-red"} />
+		</div>
+	{/if}
+</main>
+
+<style>
+	main {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	main div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	main h2 {
+		font-size: 2rem;
+		line-height: 1;
+	}
+
+	main h2,
+	main p {
+		color: var(--clr-dark-green);
+		text-align: center;
+	}
+</style>
