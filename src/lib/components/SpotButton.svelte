5775009cab2eea4d45f7665a9ca6238ece1c817d
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-09-02 20:57:05 -0700

    :construction: New button component

---
+++ src/lib/components/SpotButton.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Icon from "$lib/components/Icon.svelte";
+	export let iconName;
+	let active = false;
+</script>
+
+<button on:click={() => (active = !active)} class="spot-button" class:active>
+	<Icon name={iconName} />
+</button>
+
+<style>
+	.spot-button {
+		border: none;
+		background: var(--clr-green);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		position: relative;
+		width: 4rem;
+		transition-duration: 200ms;
+	}
+
+	.spot-button:focus {
+		outline: none;
+	}
+
+	.spot-button:before {
+		border-bottom: 35px solid var(--clr-green);
+		border-left: 2rem solid transparent;
+		border-right: 2rem solid transparent;
+		content: "";
+		height: 0;
+		left: 0;
+		position: absolute;
+		top: -34px;
+		width: 0;
+		transition-duration: 200ms;
+		transform: scaleY(0);
+		transform-origin: bottom;
+	}
+
+	.spot-button.active {
+		background: var(--clr-red);
+	}
+
+	.spot-button.active:before {
+		border-bottom: 35px solid var(--clr-red);
+		transform: scaleY(1);
+	}
+</style>
