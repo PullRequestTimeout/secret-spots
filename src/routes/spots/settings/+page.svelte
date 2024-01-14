6b2565b32b1fc3c973c2704eda6bc49dfc44505b
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-01-13 17:33:53 -0800

    :alembic: Further sort testing

--- src/routes/spots/settings/+page.svelte
+++ src/routes/spots/settings/+page.svelte
@@ -1,24 +1,32 @@
 <script>
 	import Toggle from "$lib/components/Toggle.svelte";
 	import StarRating from "$lib/components/StarRating.svelte";
-	import { sortByClosest, searchByName } from "$lib/stores/userDataStore.js";
+	import {
+		sortByClosest,
+		searchByName,
+		sortByAlphabetical,
+		sortByRecentlyAdded,
+		sortByIcon
+	} from "$lib/stores/userDataStore.js";
 	import { userSpots } from "$lib/stores/userDataStore.js";
 
 	// All the following components should populate from the database and then store values in the userPrefs store
 
 	// Search bar testing
 	let searchTerm = "";
-	$: searchTerm, searchByName($userSpots, searchTerm);
+	$: if (searchTerm.length > 0) {
+		searchByName($userSpots, searchTerm);
+	}
 
 	function sortSpots() {
-		sortByClosest($userSpots);
+		sortByIcon($userSpots);
 	}
 </script>
 
 <h2>Currently a component playground</h2>
 <Toggle />
 <StarRating />
-<button on:click={sortSpots}>Closest</button>
+<button on:click={sortSpots}>Icon</button>
 <input type="text" bind:value={searchTerm} placeholder="Search..." />
 
 <style>
