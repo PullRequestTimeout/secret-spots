63ea4a60253bb31fb21d521b64f657b5b047f547
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-01-14 17:24:52 -0800

    :bento: Added trash icon

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -89,6 +89,10 @@
 			name: "edit",
 			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M772-603 602-771l56-56q23-23 56.5-23t56.5 23l56 56q23 23 24 55.5T829-660l-57 57Zm-58 59L290-120H120v-170l424-424 170 170Z"/></svg>`
 		},
+		{
+			name: "trash",
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  width=${size} fill=${inputColor}><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
+		},
 		// Spot Icons
 		{
 			name: "cabin",
