19f217a6d1aed1e7e071c8a7097f34ca81f1b0c5
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-01-16 19:12:01 -0800

    :bento: Added new icons for sorting menu

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -93,6 +93,14 @@
 			name: "trash",
 			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  width=${size} fill=${inputColor}><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
 		},
+		{
+			name: "search",
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>`
+		},
+		{
+			name: "sort",
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z"/></svg>`
+		},
 		// Spot Icons
 		{
 			name: "cabin",
