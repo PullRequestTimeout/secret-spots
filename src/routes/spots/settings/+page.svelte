31d7d00a45c8fd1b93308a9efa38156f9127c3ae
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-01-21 19:36:32 -0800

    :construction: Clean up commit

--- src/routes/spots/settings/+page.svelte
+++ src/routes/spots/settings/+page.svelte
@@ -1,40 +1,42 @@
 <script>
 	import Toggle from "$lib/components/Toggle.svelte";
-	import StarRating from "$lib/components/StarRating.svelte";
 	import DropDown from "$lib/components/DropDown.svelte";
-	import {
-		sortByClosest,
-		searchByName,
-		sortByAlphabetical,
-		sortByRecentlyAdded,
-		sortByIcon,
-		sortByHighestRating
-	} from "$lib/stores/userDataStore.js";
+	import Chalk from "./Chalk.svelte";
 	import { userSpots } from "$lib/stores/userDataStore.js";
-
-	// All the following components should populate from the database and then store values in the userPrefs store
-
-	// Search bar testing
-	let searchTerm = "";
-	$: if (searchTerm.length > 0) {
-		searchByName($userSpots, searchTerm);
-	}
-
-	function sortSpots() {
-		sortByHighestRating($userSpots);
-	}
 </script>
 
-<h2>Currently a component playground</h2>
+<main>
+	<section>
+		<div class="settings-heading">
+			<h2>Currently a component playground</h2>
+			<div class="chalk-wrapper"><Chalk /></div>
+		</div>
+	</section>
+</main>
 <Toggle />
 <DropDown />
-<button on:click={sortSpots}>Icon</button>
-
-<!-- Move to search bar component -->
-<input class="txt-inp" type="text" bind:value={searchTerm} placeholder="Search..." />
 
 <style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	/* section{} */
+
+	.settings-heading {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+
 	h2 {
 		color: var(--clr-dark-green);
+	}
+
+	.chalk-wrapper {
+		width: 15rem;
+		translate: 0 0.1rem;
 	}
 </style>
