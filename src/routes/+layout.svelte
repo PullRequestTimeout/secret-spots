20ee5c9e55f33c0d6985ea52d6d992ef028bd015
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-02-04 13:09:59 -0800

    :recycle: Improved routing and comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,29 @@
 <script>
 	import { auth } from "$lib/firebase/firebase.js";
 	import { authStore } from "$lib/stores/authStore.js";
+	import { userSpots } from "$lib/stores/userDataStore.js";
+	import { setAlertMessage } from "$lib/stores/uiStore.js";
 
+	// Svelte methods
 	import { goto } from "$app/navigation";
 	import { onMount } from "svelte";
 	import { fade } from "svelte/transition";
 
+	// Components
+	import Alert from "$lib/components/Alert.svelte";
 	import Nav from "$lib/components/Nav.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import CloudContainer from "$lib/components/CloudContainer.svelte";
 
+	// Just used to grab the current url for the key
 	export let data;
+
 	// The only routes that don't require auth, add more to array if necessary
 	const nonAuthRoutes = ["/", "/login", "/register"];
 	onMount(() => {
 		// This function controls routing on auth state change, and sets user data in store
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
-			// Route protection for auth routes. Needs to be more robust. Move to load function?
-			const currentPath = window.location.pathname;
-			if (!user && !nonAuthRoutes.includes(currentPath)) {
-				goto("/");
-				return;
-			}
-
+			// Make sure if there is no user, dump data
 			if (!user) {
 				authStore.update(() => {
 					return {
@@ -32,8 +33,22 @@
 						emailVerified: false
 					};
 				});
+				userSpots.set([]);
+			}
+
+			// Route protection for auth routes. Needs to be more robust. Move to load function?
+			const currentPath = window.location.pathname;
+			if (!user && !nonAuthRoutes.includes(currentPath)) {
+				goto("/");
 				return;
-			} else {
+			}
+
+			if (user && auth.currentUser.emailVerified === false) {
+				goto("/register");
+				setAlertMessage("Please verify your email address to continue.");
+			}
+
+			if (user && auth.currentUser.emailVerified === true) {
 				authStore.update(() => {
 					return {
 						uid: auth.currentUser.uid,
@@ -43,13 +58,13 @@
 					};
 				});
 				goto("/spots");
-				return;
 			}
 		});
 		return unsubscribe;
 	});
 </script>
 
+<Alert />
 <Nav />
 {#key data.url}
 	<div transition:fade={{ delay: 300, duration: 200 }} class="mainContainer">
