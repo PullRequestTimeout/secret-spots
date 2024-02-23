f7ab908f8b9a8e120c5e2a94ce5bd974e8d262aa
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-02-22 18:25:43 -0800

    :fire: Removed uneeded imports

--- src/routes/spots/+layout.svelte
+++ src/routes/spots/+layout.svelte
@@ -2,23 +2,8 @@
 	import { onMount } from "svelte";
 	import { auth } from "$lib/firebase/firebase.js";
 	import { onAuthStateChanged } from "firebase/auth";
-	import { userSpots, userPref } from "$lib/stores/userDataStore.js";
-	import { checkUser, updateSpotsInDatabase, updateUserPrefInDatabase } from "$lib/firebase/db.js";
-
-	// Every time userPref store changes, update the DB
-	$: $userPref, updateUserPrefInDatabase();
-
-	// Every time spots store changes, update the DB
-	// This be an issue when deleting spots and you remove the last one
-	// This could be resolved with something like;
-
-	// let intentionalDelete = false
-	// $: if ($userSpots.length > 0 || intentionalDelete === true) {
-	// 	updateSpotsInDatabase()
-	// 	intentionalDelete = false
-	// }
-
-	// Then change intentionalDelete to true when $userSpots.length == 1 and a remove spot function is triggered
+	import { userSpots } from "$lib/stores/userDataStore.js";
+	import { checkUser, updateSpotsInDatabase } from "$lib/firebase/db.js";
 
 	$: if ($userSpots.length > 0) {
 		updateSpotsInDatabase();
